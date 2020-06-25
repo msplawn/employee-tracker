74b814cfb4359e1545fd68449bd470dbfc78e05a
msplawn <mmsplawn@gmail.com>
2020-06-24 23:29:40 -0500

    updated readme

--- db/employeeDB.sql
+++ db/employeeDB.sql
@@ -22,7 +22,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INT NOT NULL,
-  manager_id INT NOT NULL,
+  manager_id INT NULL,
   PRIMARY KEY (id)
 );
 
